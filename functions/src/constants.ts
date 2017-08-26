0d8a066ed14a53b5777c743720b8e71463ee91f9
holi0317 <holliswuhollis@gmail.com>
2017-08-26 13:13:51 +0800

    Update storage bucket

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -1,4 +1,4 @@
-export const WEBCAM_BUCKET = 'turbo-chainsaw'
+export const WEBCAM_BUCKET = 'laughing-guacamole'
 export const WEBCAM_FOLDER = 'webcam-images/'
 
 export const WEBPAGE_URL = 'https://jsse-2017.firebaseapp.com/'
